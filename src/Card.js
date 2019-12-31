a356f0201135c7170044109241fe8aa6f9988e91
Imam Yanuar <imyacloud@gmail.com>
2019-12-31 15:52:08 +0700

    profile added

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+   return (
+      <div>
+	   <img src='https://robohash.org/robofriends?200x200' alt='Robo Avatar' />
+	   <div>
+	     <h2>Jane Doe</h2>
+	     <p>jane.doe@gmail.com</p>
+	   </div>
+      </div>
+   );
+};
+
+export default Card;
